08b80fb114a98b2c0b71218f93128774c814c393
ronvave <ronvave2015@gmail.com>
2026-09-21 17:09:27 -1000

    Enable Solomon Islands public scholar submissions

--- js/solomon-submission-config.js
+++ js/solomon-submission-config.js
@@ -1,7 +1,2 @@
-/* Public, Solomon-only connection settings. Owner deployment fills these values. */
-window.SolomonSubmissionConfig = Object.freeze({
-  country: 'Solomon Islands',
-  spreadsheetId: '1um6pHKriEhbtvmkm7e8E1j0_Zt9A-oYpY88fuPoAmFY',
-  endpoint: '',
-  googleClientId: ''
-});
+/* Public Solomon Islands connection settings; no secrets. */
+window.SolomonSubmissionConfig=Object.freeze({"country":"Solomon Islands","spreadsheetId":"1um6pHKriEhbtvmkm7e8E1j0_Zt9A-oYpY88fuPoAmFY","endpoint":"https://script.google.com/macros/s/AKfycbw4iymIpI7y7ctyn3Iaufk--QIdwBlFToWwOG1CuI_EsD9YOMkNtCe7N_n_OEWhi7PyAg/exec","googleClientId":""});
